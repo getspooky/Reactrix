f29665ad116eb1452c4480eec8f594152cecfc75
yasser <spookyframework@gmail.com>
2020-09-15 12:12:31 +0200

    break: Use jest kcd-script

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  roots: ['./'],
-  testRegex: '\\.(test|spec)\\.jsx?$',
-  moduleFileExtensions: ['js', 'jsx'],
-  testPathIgnorePatterns: ['/(build|docs|node_modules|example)/'],
-  collectCoverage: true,
-  coverageReporters: ['json', 'lcov'],
-  transformIgnorePatterns: ['<rootDir>/node_modules/'],
-};
