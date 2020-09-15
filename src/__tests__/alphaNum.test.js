5b9f462bd6aa16d8e1864bad39dba05d6128dc34
yasser <spookyframework@gmail.com>
2020-09-15 23:24:13 +0200

    test(src/__tests__): Testing alphaNum validator rule

---
+++ src/__tests__/alphaNum.test.js
@@ -0,0 +1,15 @@
+import { validateRules, getStackError } from '../common';
+import alphaNum from '../alphaNum';
+
+let alphaNumRule = null;
+
+afterEach(() => alphaNumRule = null);
+
+test('validates that the string may only contains alphabetic characters', () => {
+  // valid.
+  alphaNumRule = validateRules('Hello1520', 'alphaNum');
+  expect(getStackError(alphaNumRule)).toBe(0);
+  // invalid
+  alphaNumRule = validateRules('@123','alphaNum');
+  expect(getStackError(alphaNumRule)).toBe(1);
+});
