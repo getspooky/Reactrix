ce3742fb3a5be8d374599452f9a6ad0ecec8a052
yasser <spookyframework@gmail.com>
2020-09-15 21:51:58 +0200

    refactor(src): Use isObject util function

--- src/object.js
+++ src/object.js
@@ -1,3 +1,4 @@
 // Copyright 2020 the Reactrix authors. All rights reserved. MIT license.
 import { assertExp } from './common';
-export default assertExp('object', (val) => typeof val === 'object');
+import { isObject } from './utils/types';
+export default assertExp('object', (val) => isObject(val));
