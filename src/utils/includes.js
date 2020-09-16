425709d879f1c9003901462aed795bc63b865738
yasser <spookyframework@gmail.com>
2020-09-16 18:52:27 +0200

    fix(src/utils): Export includes function

--- src/utils/includes.js
+++ src/utils/includes.js
@@ -1,3 +1,2 @@
 // Copyright 2020 the Reactrix authors. All rights reserved. MIT license.
-const includes = (arr, val) => arr.some(arrVal => val === arrVal);
-export default includes;
+export const includes = (arr, val) => arr.some(arrVal => val === arrVal);
