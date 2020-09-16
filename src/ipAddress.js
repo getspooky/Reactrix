3196486577a61a1e72417d85ae25e7be972447f2
yasser <spookyframework@gmail.com>
2020-09-16 13:25:55 +0200

    fix(src): emailRegex not defined

--- src/ipAddress.js
+++ src/ipAddress.js
@@ -1,4 +1,4 @@
 // Copyright 2020 the Reactrix authors. All rights reserved. MIT license.
 import { regex } from './common';
 const ipAddressRegex = /^(?!0)(?!.*\.$)((1?\d?\d|25[0-5]|2[0-4]\d)(\.|$)){4}$/;
-export default regex('ipAddress', emailRegex);
+export default regex('ipAddress', ipAddressRegex);
