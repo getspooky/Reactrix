e35035e6ad13e510c3df15f05698748d2e7a9c17
yasser <spookyframework@gmail.com>
2020-09-16 18:43:41 +0200

    test(src/currency/__tests__): Testing ehereum address validator rule

---
+++ src/currency/__tests__/ethereumAddress.test.js
@@ -0,0 +1,11 @@
+import { validateRules, getStackError } from '../../common';
+
+let ethereumAddressRule = null;
+
+afterEach(() => ethereumAddressRule = null);
+
+test('validates that the string is a valid Ethereum address', () => {
+  // valid.
+  ethereumAddressRule = validateRules('0x89205A3A3b2A69De6Dbf7f01ED13B2108B2c43e8', 'currency/ethereum');
+  expect(getStackError(ethereumAddressRule)).toBe(0);
+});
