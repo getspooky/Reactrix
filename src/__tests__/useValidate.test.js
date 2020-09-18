1fa6d290a2b76477ea046f3796b5a9d5511dcc04
yasser <spookyframework@gmail.com>
2020-09-18 19:37:08 +0200

    test(src/__tests__): Testing useValidator component

---
+++ src/__tests__/useValidate.test.js
@@ -0,0 +1,37 @@
+// Copyright 2020 the Reactrix authors. All rights reserved. MIT license.
+import { renderHook, act } from '@testing-library/react-hooks'
+import { useValidate } from '../useValidate';
+
+var data = {
+  email: 'example@github.com',
+  pass : '1234456'
+};
+
+afterEach(() => data = null);
+
+test('Should return empty array with no error', () => {
+  const { result } = renderHook(() => useValidate());
+  act(() => {
+    result.current[1](data, {
+      email: 'required|email',
+      pass: 'required|string'
+    });
+  });
+  expect(result.current[0].length).toBe(0);
+});
+
+test('Should return array with message', () => {
+  const { result } = renderHook(() => useValidate());
+  data = {
+    pass: 12333,
+    email: 123,
+  };
+  act(() => {
+    result.current[1](data, {
+      email: 'string',
+      pass:  'required|string'
+    });
+  });
+  expect(result.current[0].length).toBe(2);
+});
+
