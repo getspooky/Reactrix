5971646b2a56bc31c8fabcc9f6c2b4d0271be98d
yasser <spookyframework@gmail.com>
2020-09-22 22:03:25 +0200

    test(src/__tests__): Should not throw error if the lang is not string

--- src/__tests__/useValidate.test.js
+++ src/__tests__/useValidate.test.js
@@ -21,7 +21,7 @@ test('Should return empty array with no error', () => {
 });
 
 test('Should return array with message', () => {
-  const { result } = renderHook(() => useValidate());
+  const { result } = renderHook(() => useValidate({}));
   data = {
     pass: 12333,
     email: 123,
@@ -39,6 +39,19 @@ test('Should return message with supported locales', () => {
   const { result } = renderHook(() => useValidate('en'));
   data = {
     name: 12333,
+  };
+  act(() => {
+    result.current[1](data, {
+      name: 'string',
+    });
+  });
+  expect(result.current[0]).toContain('The name field must be a string');
+});
+
+test('Should not throw error if the lang is not string', () => {
+  const { result } = renderHook(() => useValidate({}));
+  data = {
+    name: 3233,
   };
   act(() => {
     result.current[1](data, {
